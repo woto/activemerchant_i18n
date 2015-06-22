ae4a1b1a6c89e3d0e1a55f87bc5f03b1bd689c4a
Ruslan Kornev <oganer@gmail.com>
2015-06-23 03:39:31 +0500

    Работает

--- activemerchant_i18n.gemspec
+++ activemerchant_i18n.gemspec
@@ -17,6 +17,8 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency "rails", "~> 3.2.22"
+  s.add_dependency "activemerchant", "1.31.1"
+  s.add_dependency "i18n"
 
   s.add_development_dependency "sqlite3"
 end
--- lib/activemerchant_i18n.rb
+++ lib/activemerchant_i18n.rb
@@ -1,2 +1,26 @@
 module ActivemerchantI18n
 end
+
+path = File.dirname(__FILE__)
+I18n.load_path += Dir[ File.join(path, '../config/locales', '*.{rb,yml}') ]
+
+module ActiveMerchant #:nodoc:
+  module Validateable #:nodoc:
+
+    def valid_with_i18n?
+      valid_without_i18n?
+      result = {}
+      errors.each do |k, errors_array|
+        result[k] = []
+        errors_array.each do |error_string|
+          result[k] << I18n.translate(error_string.parameterize.underscore, scope: 'activemerchant')
+        end
+      end
+      @errors = result
+      return errors.empty?
+    end
+
+    alias_method_chain :valid?, :i18n
+
+  end
+end
